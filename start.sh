e4d8b4a466e96d6fb75737a633bcb1bf8ff7e97d
maximing <maximing666@126.com>
2025-09-03 20:40:20 +0800

    处理migrate时，表已经存在的问题。

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 apt-get update && apt-get install -y telnet
 cd /opt/conda/vstock/ && conda install --file requirements.txt
 python manage.py makemigrations
-python manage.py migrate
+#--fake-initial 检查数据库，如果表已存在且结构与模型匹配，就跳过创建，只标记迁移状态
+python manage.py migrate --fake-initial  
 python manage.py runserver 0.0.0.0:18001
